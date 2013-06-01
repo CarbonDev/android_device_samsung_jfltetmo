ccefb1912891f18da8c9a79ed9d0b3fdf50a6b8d
nocoast <voteforpedro8@gmail.com>
2013-06-01 18:37:20 -0500

    Tmo

--- carbon.mk
+++ carbon.mk
@@ -1,16 +1,13 @@
 $(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
 
-PRODUCT_NAME := cm_jfltetmo
+PRODUCT_NAME := carbon_jfltetmo
 PRODUCT_DEVICE := jfltetmo
 
