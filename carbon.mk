9f6a12c8cb4315878ebff12e7bc54f9081ddd2c9
bigshotrob22 <rrascon22@gmail.com>
2013-10-30 14:56:32 -0700

    Remove common_gsm.mk
    Change-Id: I60dbbe9f641d78eb4fddd30aca34430b80a2a2ca

--- carbon.mk
+++ carbon.mk
@@ -1,9 +1,6 @@
 $(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/carbon/config/common_gsm.mk)
-
-# Inherit some common CM stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
