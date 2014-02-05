a4f3e2785b968ea2fc90eb17f2bb838ca99cf450
Patrick Campanale <patrickappcreator@gmail.com>
2014-02-05 14:11:09 -0600

    Changed common_full_phone.mk to common_phone.mk

--- carbon.mk
+++ carbon.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
 #$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.3/JSS15J/M919UVUEMK2:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.3 JSS15J M919UVUEMK2 release-keys"
 
